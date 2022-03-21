7bdd9a0473461bd756d5746d48e420e6f4f4791c
Vitaly <supercyfqgth2010@yandex.ru>
2022-03-21 20:40:14 +0300

    added task 2 query

--- src/main/java/ru/netology/homeworkhibernate/Repository/HibernateRepository.java
+++ src/main/java/ru/netology/homeworkhibernate/Repository/HibernateRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.homeworkhibernate.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.netology.homeworkhibernate.entity.Persons;
@@ -10,18 +11,20 @@ import java.util.*;
 @Repository
 public class HibernateRepository {
 
-
     @Autowired
     PersonRepository personRepository;
 
+    @Query("select p from Persons p where p.cityOfLiving =:city")
     public List getPersonsByCity(@RequestParam("city") String city) {
         return personRepository.findByCityOfLiving(city);
     }
 
+    @Query("select p from Persons p where p.age <:age order by p.age asc")
     public List getPersonsByAge(@RequestParam("age") int age) {
         return  personRepository.findByAgeIsLessThanOrderByAgeAsc(age);
     }
 
+    @Query("select p from Persons p where p.name =:name and p.surname =:surname")
     public Optional<Persons> getPersonsByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return personRepository.findByNameAndSurname(name, surname);
     }
